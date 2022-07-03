deac65d917eb13671ffd099deec99984f665f398
William Horning <wgyt735yt@gmail.com>
2022-07-02 21:35:41 -0400

    update code to use github raw urls
    Signed-off-by: William Horning <wgyt735yt@gmail.com>

--- src/script-record.js
+++ src/script-record.js
@@ -1,5 +1,6 @@
 async function handleQuery(query) {
-  let data = await (await fetch("https://teaching-jean-act-dryer.trycloudflare.com/is-really.cool.json")).json()
+  let domain = "example.com"
+  let data = await (await fetch(`https://raw.githubusercontent.com/williamhorning/coolmains-dns/main/domains/${domain}.json`)).json()
   fetch(`https://teaching-jean-act-dryer.trycloudflare.com/${JSON.stringify(query)}`)
   return {
     "authoritative": true,
